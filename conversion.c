e2b5a20364b307212e52508afb5a2914ca296a72
soumyajeet-kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-02-12 13:43:54 +0530

    logical change

--- conversion.c
+++ conversion.c
@@ -42,7 +42,7 @@ void convertFahrenheitToKelvin(float fahrenheit, float subtraction, float kelvin
     printf("Enter a temp in fahrenheit: ");
     scanf("%f", &fahrenheit);
     subtraction = fahrenheit - 32;
-    kelvin = (subtraction * 5) / 9 + 273.15;
+    kelvin =((subtraction * 5) / 9) + 273.15;
     printf("%.2fF is same as %.2fK", fahrenheit, kelvin);
 }
 
