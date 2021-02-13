a6048c33ae395f20e0523b6754f96928d9d15881
PS99003576 <roshanzameer.shaik@ltts.com>
2021-02-13 14:08:10 +0530

    updated test functions

---
+++ Implementation/test/test_convertFahrenheitToKelvin.c
@@ -0,0 +1,24 @@
+#include "unity.h"
+#include "convertFahrenheitToKelvin.h"
+#include "test_convertFahrenheitToKelvin.h"
+
+
+void test_convertFahrenheitToKelvin_basic(void)
+{
+    // H_01
+    int h01_arr[] = {40};
+    int h01_n = sizeof(h01_arr)/sizeof(int);
+	TEST_ASSERT_EQUAL( 277.594  , convertFahrenheitToKelvin(h01_n, (int*)&h01_arr));
+}
+
+int test_convertFahrenheitToKelvin(void)
+{
+	/* Initiate the Unity Test Framework */
+	UNITY_BEGIN();
+
+	/* Run Test functions */
+	RUN_TEST(test_convertFahrenheitToKelvin_basic);
+
+	/* Close the Unity Test Framework */
+	return UNITY_END();
+}
---
+++ Implementation/test/test_convertFahrenheitToKelvin.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_convertFahrenheitToKelvin_H__
+#define __TEST_convertFahrenheitToKelvin_H__
+
+int test_convertFahrenheitToKelvin(void);
+
+#endif /* #ifndef __convertFahrenheitToKelvin_H__ */
