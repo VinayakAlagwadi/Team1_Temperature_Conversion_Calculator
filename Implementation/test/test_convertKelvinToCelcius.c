abab8b0aa5a9e865ee7a71b625b561a9f732e878
PS99003576 <roshanzameer.shaik@ltts.com>
2021-02-15 09:04:25 +0530

    updated module

--- Implementation/test/test_convertKelvinToCelcius.c
+++ Implementation/test/test_convertKelvinToCelcius.c
@@ -1,12 +1,15 @@
 #include "unity.h"
 #include "convertKelvinToCelcius.h" /* The unit to be tested. */
+#include "test_convertKelvinToCelcius.h"
 
 void setUp (void) {} /* Is run before every test, put unit init calls here. */
 void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
 
 void test_convertKelvinToCelcius(void)
 {
-   TEST_IGNORE_MESSAGE("Hello world!"); /* Ignore this test but print a message. */
+   int h01_arr[] = {0,0,0};
+    int h01_n = sizeof(h01_arr)/sizeof(int);
+   TEST_ASSERT_EQUAL( 273.15, convertKelvinToCelcius(h01_n, (int*)&h01_arr));
 }
 
 int main (void)
--- Implementation/test/test_convertKelvinToFahrenheit.c
+++ Implementation/test/test_convertKelvinToFahrenheit.c
@@ -8,8 +8,8 @@ void test_convertKelvinToFahrenheit(void)
 {
    int h01_arr[] = {255.928,0,0};
     int h01_n = sizeof(h01_arr)/sizeof(int);
-    UNITY_TEST_ASSERT_EQUAL_INT((1), (convertKelvinTofahrenheit(h01_n, (int*)&h01_arr)), __LINE__, NULL)
-	//TEST_ASSERT_EQUAL( 1, convertKelvinTofahrenheit(h01_n, (int*)&h01_arr));
+    UNITY_TEST_ASSERT_EQUAL_INT((1), (convertKelvinToFahrenheit(h01_n, (int*)&h01_arr)), __LINE__, NULL)
+	//TEST_ASSERT_EQUAL( 1, convertKelvinToFahrenheit(h01_n, (int*)&h01_arr));
 }
 
 int main (void)
