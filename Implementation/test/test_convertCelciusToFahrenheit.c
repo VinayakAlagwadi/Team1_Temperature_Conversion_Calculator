394071d6e7ea294dbb422c1a5e68ac313b831c81
PS99003576 <roshanzameer.shaik@ltts.com>
2021-02-15 09:07:56 +0530

    Update test_convertCelciusToFahrenheit.c

--- Implementation/test/test_convertCelciusToFahrenheit.c
+++ Implementation/test/test_convertCelciusToFahrenheit.c
@@ -1,6 +1,7 @@
 
 #include "unity.h"
 #include "convertCelciusToFahrenheit.h" /* The unit to be tested. */
+#include "test_convertCelciusToFahrenheit.h"
 
 void setUp (void) {} /* Is run before every test, put unit init calls here. */
 void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
