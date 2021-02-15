d26936fc8fb0f359f165235b5525533a492a6c93
VinayakAlagwadi <78849168+VinayakAlagwadi@users.noreply.github.com>
2021-02-15 09:18:17 +0530

    Delete conversion.d

--- extra/conversion.d
+++
@@ -1,8 +0,0 @@
-obj/conversion.o: \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/conversion.c \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertCelciusToFahrenheit.h \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertFahrenheitToCelcius.h \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertCelciusToKelvin.h \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertKelvinToCelcius.h \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertFahrenheitToKelvin.h \
- C:\Users\Mahavir\Documents\GitHub\Team1_Temperature_Conversion_Calculator\Implementation/inc/convertKelvinTofahrenheit.h
